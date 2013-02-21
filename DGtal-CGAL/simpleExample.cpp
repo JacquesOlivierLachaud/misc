88ba2d2aacb3a21e261cb6031a8c03fc388aea04
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2013-02-21 14:03:41 +0100

    Add visualization of area 1 triangles.

--- DGtal-CGAL/simpleExample.cpp
+++ DGtal-CGAL/simpleExample.cpp
@@ -6,6 +6,8 @@
 #include <DGtal/topology/helpers/Surfaces.h>
 #include <DGtal/geometry/curves/GridCurve.h>
 #include <DGtal/io/boards/Board2D.h>
+#include <DGtal/geometry/curves/ArithmeticalDSS.h>
+#include <DGtal/geometry/curves/SaturatedSegmentation.h>
 
 #include <CGAL/Delaunay_triangulation_2.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
@@ -17,6 +19,7 @@ typedef Delaunay::Vertex_circulator Vertex_circulator;
 typedef Delaunay::Edge_iterator  Edge_iterator;
 typedef Delaunay::Finite_faces_iterator  Faces_iterator;
 typedef Delaunay::Point             Point;
+typedef Delaunay::Face_handle  Face_handle;
 
 
 DGtal::Z2i::Point toDGtal(const Point &p)
@@ -30,8 +33,15 @@ int main ()
   Delaunay t;
   
   trace.beginBlock("Construction the shape");
+  // typedef Ellipse2D<Z2i::Space> Ellipse; 
+  // Ellipse2D<Z2i::Space> ellipse(Z2i::Point(0,0), 5, 3, 0.3 );
+  // double h = 1; 
+  // GaussDigitizer<Z2i::Space,Ellipse> dig;  
+  // dig.attach( ellipse );
+  // dig.init( ellipse.getLowerBound()+Z2i::Vector(-1,-1),
+  //           ellipse.getUpperBound()+Z2i::Vector(1,1), h ); 
   typedef Flower2D<Z2i::Space> Flower; 
-  Flower2D<Z2i::Space> flower(Z2i::Point(0,0), 200, 50, 5, 0);
+  Flower2D<Z2i::Space> flower(Z2i::Point(0,0), 15, 2, 5, 0);
   double h = 1; 
   GaussDigitizer<Z2i::Space,Flower> dig;  
   dig.attach( flower );
@@ -62,32 +72,82 @@ int main ()
   std::cout << "number of faces :  " ;
   std::cout << t.number_of_faces() << std::endl;
   
+  // GridCurve
+  Z2i::Curve gc;
+  gc.initFromPointsRange( r.begin(), r.end() );
+  typedef Z2i::Curve::PointsRange::ConstIterator ConstIterator;
+  typedef ArithmeticalDSS<ConstIterator,int,4> DSS4;
+  typedef SaturatedSegmentation<DSS4> Segmentation;
+  //Segmentation
+  Z2i::Curve::PointsRange range = gc.getPointsRange();
+  DSS4 dss4RecognitionAlgorithm;
+  Segmentation theSegmentation( range.begin(), range.end(), dss4RecognitionAlgorithm );
+         
+
   DGtal::Board2D board;
   
+  Z2i::Point dP;
+  board << CustomStyle( dP.className(), 
+                        new CustomPen( Color(0,0,0), Color(230,230,230), 1, 
+                                       Board2D::Shape::SolidStyle,
+                                       Board2D::Shape::RoundCap,
+                                       Board2D::Shape::RoundJoin ));
   for(Range::ConstIterator it=r.begin(), itend=r.end(); it != itend;
       ++it)
     board << *it;
   
-  Z2i::Point dP;
-  board << CustomStyle( dP.className(), 
-                        new CustomPen( Color(0,0,0), Color(180,18,18), 1, 
-                                       Board2D::Shape::SolidStyle,
-                                       Board2D::Shape::RoundCap,
-                                       Board2D::Shape::RoundJoin ));
-    
-  board.setPenColor(DGtal::Color::Red);
-  board.setFillColorRGBi(255,255,255,0);
   for(Faces_iterator it = t.finite_faces_begin(), itend=t.finite_faces_end();
       it != itend; ++it)
     {
       Z2i::Point a( toDGtal(it->vertex(0)->point())),
 	b(toDGtal(it->vertex(1)->point())),
 	c(toDGtal(it->vertex(2)->point()));
-      
-      board.drawTriangle(a[0],a[1],b[0],b[1],c[0],c[1]);
+
+      Z2i::Vector ab( b - a ), ac( c - a );
+      int d = ab[ 0 ] * ac[ 1 ] - ab[ 1 ] * ac[ 0 ];
+      if ( ( d == 1 ) || (d == -1 ) )
+        {
+          board.setPenColor(DGtal::Color::Blue);
+          board.setFillColor( DGtal::Color::None );
+          board.setLineWidth( 2.0 );
+          board.drawTriangle(a[0],a[1],b[0],b[1],c[0],c[1]);
+        }
+      else
+        {
+          board.setPenColor(DGtal::Color::Red);
+          board.setFillColor( DGtal::Color::None );
+          //          board.setFillColorRGBi(200,200,200,128);
+          board.setLineWidth( 1.0 );
+          board.drawTriangle(a[0],a[1],b[0],b[1],c[0],c[1]);
+        }
+    }
+
+  Segmentation::SegmentComputerIterator i = theSegmentation.begin();
+  Segmentation::SegmentComputerIterator end = theSegmentation.end();
+  board.setPenColor(DGtal::Color::Green);
+  board.setFillColor( DGtal::Color::None );
+  board << SetMode( "ArithmeticalDSS", "BoundingBox" );
+  string aStyleName = "ArithmeticalDSS/BoundingBox";
+  for ( ; i != end; ++i) {
+    DSS4 current(*i);
+    board << CustomStyle( aStyleName, 
+                          new CustomPenColor( DGtal::Color::Green ) )
+          << current;
+  } 
+
+  for(Edge_iterator it = t.edges_begin(), itend=t.edges_end();
+      it != itend; ++it)
+    {
+      // vertex(cw(i)) and vertex(ccw(i)) of f.
+      Face_handle itf = it->first;
+      int i = it->second;
+      Z2i::Point a( toDGtal(itf->vertex( t.cw( i ) )->point()));
+      Z2i::Point b( toDGtal(itf->vertex( t.ccw( i ) )->point()));
+      std::cout << "Edge: " << a << " -> " << b << std::endl;
     }
   
   board.saveSVG("delaunay.svg");
+  board.saveEPS("delaunay.eps");
   
   return 0;
 }
