e1c825e61d0992557cfc96c5eefa48d9aa3a7a8e
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2018-02-07 21:06:38 +0100

    Fix subdivision in tv-triangulation-color

--- DGtal-CGAL/bezier-triangle.sage
+++ DGtal-CGAL/bezier-triangle.sage
@@ -14,10 +14,23 @@ M = matrix(
 B = vector( [a,b,c,0,0,0] )
 X = M.inverse() * B
 # (a, b, c, -1/2*a*((d0 + e0)/e0 + (d2 - (d0 + e0)*e2/e0)/e2) + 1/2*b*(d0/e0 + (d1 - d0*e1/e0 + e1)/e1) - 1/2*c*(d1/e1 - (d2 + e2)/e2), -1/2*b*d0/e0 - 1/2*a*(d2 - (d0 + e0)*e2/e0)/e2 + 1/2*c*(d2 + e2)/e2, 1/2*a*(d0 + e0)/e0 + 1/2*b*(d1 - d0*e1/e0 + e1)/e1 - 1/2*c*d1/e1)
-f(d0,e0,d1,e1,d2,f2)=(a, b, c, -1/2*a*((d0 + e0)/e0 + (d2 - (d0 + e0)*e2/e0)/e2) + 1/2*b*(d0/e0 + (d1\
+f(a,b,c,d0,e0,d1,e1,d2,e2)=(a, b, c, -1/2*a*((d0 + e0)/e0 + (d2 - (d0 + e0)*e2/e0)/e2) + 1/2*b*(d0/e0 + (d1\
  - d0*e1/e0 + e1)/e1) - 1/2*c*(d1/e1 - (d2 + e2)/e2), -1/2*b*d0/e0 - 1/2*a*(d2 - (\
 d0 + e0)*e2/e0)/e2 + 1/2*c*(d2 + e2)/e2, 1/2*a*(d0 + e0)/e0 + 1/2*b*(d1 - d0*e1/e0\
  + e1)/e1 - 1/2*c*d1/e1)
 
-f(-1/2,1,-1/2,1,-1/2,1)
+# Triangle coin bas gauche avec gradients orth sur les trois arêtes
+f(a,b,c,0,1,1/2,-1,-1,1)
+
+b = vector( f(a,b,c,0,1,1/2,-1,-1,1) )
+var('r','s','t')
+R(x,y)=1-x-y
+S(x,y)=x
+T(x,y)=y
+P(x,y)=R(x,y)^2*b[0] + S(x,y)^2 * b[1] + T(x,y)^2 * b[ 2 ] + R(x,y)*S(x,y) * b[3] + S(x,y)*T(x,y) * b[ 4 ] + T(x,y)*R(x,y) * b[5]
+
+Q(a,b,c,x,y)=a*(x + y - 1)^2 - 1/4*(2*a + b + c)*(x + y - 1)*x + b*x^2 - 1/4*(2*a + b + c)*(x + y - 1)*y + a*x*y + c*y^2
+
+Q(1,0,0,x,y)
+(x + y - 1)^2 - 1/2*(x + y - 1)*x - 1/2*(x + y - 1)*y + x*y
 
--- DGtal-CGAL/testBezierTriangle2.cpp
+++ DGtal-CGAL/testBezierTriangle2.cpp
@@ -7,6 +7,7 @@
 #include <DGtal/base/Alias.h>
 #include <DGtal/helpers/StdDefs.h>
 #include "BezierTriangle2.h"
+#include "BezierTriangle3.h"
 #include "CairoViewer.h"
 
 int main( int argc, char** argv )
@@ -14,23 +15,43 @@ int main( int argc, char** argv )
   using namespace DGtal;
   using namespace Z2i;
 
-  typedef BezierTriangle2< Space, 3 > BT;
+  typedef BezierTriangle3< Space, 3 > BT;
   typedef BT::Value                   Value;
   typedef CairoViewer< Space >        Viewer;
-  RealPoint a( 0, 0 );
-  RealPoint b( 10, 0 );
-  RealPoint c( 0, 10 );
-  RealVector o_ab( 0, 1 );
-  RealVector o_bc( 1, 1 );
-  RealVector o_ca( 1, 1 );
+  RealPoint a( 10, 0 );
+  RealPoint b( 20, 0 );
+  RealPoint c( 10, 10 );
+  RealPoint d( 0, 5 );
   Value va( 255, 255, 255 );
-  Value vb( 0, 255, 0 );
-  Value vc( 0, 0, 255 );
-  BT bt( { a, b, c }, { o_ab, o_bc, o_ca }, { va, vb, vc } );
+  Value vb( 0, 0, 0 );
+  Value vc( 0, 0, 0 );
+  Value vd( 100, 100, 100 );
+  Value vabc = BT::midValue( { va, vb, vc } );
+  Value vacd = BT::midValue( { va, vc, vd } );
+  RealVector o_ab = 0.5 * BT::grad( { a, b, c }, { va[ 0 ], vb[ 0 ], vc[ 0 ] } )
+    + 0.5 * BT::grad( { b, a, RealPoint( 15, -5 ) }, { vb[ 0 ], va[ 0 ], 85 } );
+  RealVector o_bc = 0.5 * BT::grad( { a, b, c }, { va[ 0 ], vb[ 0 ], vc[ 0 ] } )
+    + 0.5 * BT::grad( { c, b, RealPoint( 18, 8 ) }, { vc[ 0 ], vb[ 0 ], 0 } );
+  RealVector o_ca = 0.5 * BT::grad( { a, b, c }, { va[ 0 ], vb[ 0 ], vc[ 0 ] } )
+    + 0.5 * BT::grad( { a, c, (a+c+d/3.0) }, { va[ 0 ], vc[ 0 ], vacd[ 0 ] } );
+  std::cout << "o_ab=" << o_ab << std::endl;
+  std::cout << "o_bc=" << o_bc << std::endl;
+  std::cout << "o_ca=" << o_ca << std::endl;
+  BT bt1( { a, b, c }, { va, vb, vc },
+	  { o_ab, o_bc, o_ca },
+	  vabc,
+	  { RealPoint( 15, -5 ), RealPoint( 18, 8 ), (a+c+d)/3.0 },
+	  { Value( 85, 85, 85 ), Value( 0, 0, 0 ), vacd } );
+  // BT bt2( { a, c, d }, { va, vc, vd },
+  // 	  { -o_ca, o_cd, o_da },
+  // 	  vacd,
+  // 	  { (a+b+c)/3.0, RealPoint( 5, 10 ), RealPoint( 4, 0 ) }, 
+  // 	  { vabc, Value( 20, 20, 20 ), Value( 40, 40, 40) } );
 
-  Viewer viewer( 0.0, 0.0, 15.0, 15.0, 16.0, 16.0 );
+  Viewer viewer( 0.0, 0.0, 20.0, 10.0, 4.0, 4.0 );
   //viewer.drawLinearGradientTriangle( a, b, c, va, vb, vc );
-  viewer.drawBezierTriangle( bt );
+  viewer.drawColorBezierTriangle( bt1 );
+  // viewer.drawColorBezierTriangle( bt2 );
   viewer.save( "bt.png" );
 
   return 0;
